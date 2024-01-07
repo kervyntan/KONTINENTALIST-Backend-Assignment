346d56cc83d7a57aeab543ee5b01bed07456afe8
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-01-07 15:31:20 +0800

    refactored connecting to client and querying into a function

--- routes/posts.js
+++ routes/posts.js
@@ -5,25 +5,31 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.CONNECTION_STRING,
   ssl: {
-    rejectUnauthorized : false
+    rejectUnauthorized: false,
+  },
+});
+
+const queryPostgres = async (query = "SELECT * FROM posts") => {
+    const client = await pool.connect();
+    const result = await client.query(query);
+    client.release();
+    return result.rows;
+}
+
+router.get("/", async (req, res) => {
+  try {
+    const results = await queryPostgres();
+    res.send(results);
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
   }
 });
 
-router.get("/", (req, res) => {
-    console.log("hey")
-    res.send({ success: true })
-});
-
-router.get("/db", async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT * FROM posts");
-    const results = {
-      results: result ? result.rows : null,
-    };
-    console.log(results);
-    client.release();
-    res.send({ success: true })
+    const results = await queryPostgres(`SELECT * FROM posts WHERE id = ` + req.params.id);
+    res.send(results);
   } catch (err) {
     console.error(err);
     res.send("Error " + err);
